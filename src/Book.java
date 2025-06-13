387fbe415cfb181ab39fa98ed8f94e3d1d424004
aayush-navsariwala <aayush.nav@gmail.com>
2025-06-13 20:28:16 +0200

    Commit 8 - Added comments to Book.java class

--- src/Book.java
+++ src/Book.java
@@ -1,7 +1,9 @@
 public class Book {
+    //Fields to store book information
     private String title, author, publisher;
     private int year;
 
+    //Constructor to initialise a Book object with its details
     public Book(String title, String author, String publisher, int year) {
         this.title = title;
         this.author = author;
@@ -9,11 +11,13 @@ public class Book {
         this.year = year;
     }
 
+    //Getter methods for title, author, publisher and year
     public String getTitle() { return title; }
     public String getAuthor() { return author;}
     public String getPublisher() { return publisher;}
     public int getYear() { return year;}
 
+    //Overrides the default toString() method to provide a readable string
     @Override
     public String toString() {
         return String.format(
